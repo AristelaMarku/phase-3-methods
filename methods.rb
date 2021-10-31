b9b2085900ef08407a59770dab8515f3513e27f1
Aristela Marku <aristela.marku@gmail.com>
2021-10-31 21:44:31 +0100

    commit tree 683f322bb1b27c6c5f3c445bdd1773e48a71c29e

--- methods.rb
+++ methods.rb
@@ -1,1 +1,35 @@
-# Your code here!
+
+def greet_programmer
+    puts  "Hello, programmer!"
+end
+greet_programmer
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+greet "Aristela"
+
+def greet_with_defaul(name="programmer")
+    puts " Hello, #{name}! "
+end
+
+greet_with_defaul "Naureen"
+greet_with_defaul 
+
+
+def add num1,num2
+    num1+num2
+end
+
+add 2,3
+
+def halve num1
+    if num1.class != Integer
+        return nil
+    end
+    num1/2
+end
+halve 6
+
+
